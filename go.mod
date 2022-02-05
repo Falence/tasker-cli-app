ebfe84568796e806e4fb6b00ede0fd44cf5670c0
Falence Lemungoh <falencelemungoh@gmail.com>
2022-02-05 23:23:59 +0100

    creating a cli program

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/falence/tasker
 
 go 1.16
 
-require go.mongodb.org/mongo-driver v1.8.3 // indirect
+require (
+	github.com/urfave/cli/v2 v2.3.0
+	go.mongodb.org/mongo-driver v1.8.3
+)
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
+	"github.com/urfave/cli/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -24,4 +26,17 @@ func init() {
 	}
 
 	collection = client.Database("tasker").Collection("tasks")
+}
+
+func main() {
+	app := &cli.App{
+		Name:     "tasker",
+		Usage:    "A simple CLI program to manage your tasks",
+		Commands: []*cli.Command{},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
